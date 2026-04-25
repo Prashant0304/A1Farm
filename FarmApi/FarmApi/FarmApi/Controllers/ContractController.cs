5e83627f43435b0a27d319373a0a9eb0f47d2d65
Guna1551 <gunashekhar1501@gmail.com>
2026-04-25 13:09:31 +0530

    Added Db Context

---
+++ FarmApi/FarmApi/FarmApi/Controllers/ContractController.cs
@@ -0,0 +1,48 @@
+﻿using FarmApi.Models.DTO;
+using FarmApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+
+namespace FarmApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractController : ControllerBase
+    {
+        private readonly DbService _db;
+
+        public ContractController(DbService db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string lang = "en", int pageNumber = 1, int pageSize = 10)
+        {
+            var (data, total) = await _db.QueryPagedAsync<dynamic>(
+                "GetContracts",
+                new { LanguageCode = lang, PageNumber = pageNumber, PageSize = pageSize });
+
+            return Ok(new { data, total });
+        }
+
+        [HttpPost("save-contract")]
+        public async Task<IActionResult> Save([FromBody] dynamic model)
+        {
+            await _db.ExecuteAsync("SaveContract", model);
+            return Ok();
+        }
+
+        [HttpGet("get-landid")]
+
+        public async Task<IActionResult> GetLandIdByFarmerId(int farmerId)
+        {
+            var lands = await _db.QueryAsync<dynamic>(
+                 "GetLandsByFarmerId",
+                 new { FarmerId = farmerId });
+
+            return Ok(lands);
+        }
+    }
+}
---
+++ FarmApi/FarmApi/FarmApi/Services/FarmDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FarmApi.Services
+{
+    public class FarmDbContext: DbContext
+    {
+        public FarmDbContext(DbContextOptions<FarmDbContext> options)
+        : base(options)
+        {
+        }
+
+        // Only needed if you map result sets
+        //public DbSet<Farmer> Farmers { get; set; }
+    }
+}
